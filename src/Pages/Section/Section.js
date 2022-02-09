5ddcfe291102cc2c2eeb6d6ea60ef6d54be6a7dc
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2022-02-09 17:20:32 -0600

    added animated cards

--- src/Pages/Section/Section.js
+++ src/Pages/Section/Section.js
@@ -1,50 +1,51 @@
-import "./Section.css";
+import style from "./Section.module.css";
 import Card from "../../components/Cards/Card";
+import React, { useState } from "react";
+import styledEngine from "@mui/styled-engine";
+
 const services = {
   paint: {
     title: "Paint",
     description: "Paint your vehicle with our quality paint.",
     image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFEXHhrxQo7y7l9OL5ycEy_tvrVm8lxqntfg&usqp=CAU",
+      "https://scontent-dfw5-2.xx.fbcdn.net/v/t1.18169-9/21617620_1851544888418093_3964336788619649675_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=730e14&_nc_ohc=HKEnd_8AoGoAX9YBI20&_nc_ht=scontent-dfw5-2.xx&oh=00_AT9XVpt1SFtbcZhWgEAy9jXIrbNUcqi9mJCUuREKreablA&oe=6228FC3F",
   },
   body: {
     title: "Body",
     description: "Body your vehicle with our quality paint.",
     image:
-      "https://static.vecteezy.com/system/resources/thumbnails/003/694/243/small/car-icon-in-flat-style-simple-traffic-icon-free-vector.jpg",
+      "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.6435-9/94952792_2474149362824306_275763464775925760_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=730e14&_nc_ohc=P8GOuJD_OFYAX-rJfJ9&tn=pHZFH3cZ5dZFEVK8&_nc_ht=scontent-dfw5-1.xx&oh=00_AT-NRCSYpvrwofieCsCReOk7w6--K6v1jWVGCuf-oLXs2w&oe=622A00AB",
   },
   detailing: {
     title: "Detailing",
     description: "Detailing your vehicle with our quality paint.",
     image:
-      "https://st2.depositphotos.com/1029662/45205/v/600/depositphotos_452051896-stock-illustration-professional-auto-car-detailer-icons.jpg",
+      "https://scontent-dfw5-2.xx.fbcdn.net/v/t1.6435-9/95257941_2474149356157640_6454232370005409792_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=730e14&_nc_ohc=s0h--SUdTNEAX8_umi-&_nc_ht=scontent-dfw5-2.xx&oh=00_AT-lhiYkTS1eV2xuOcPs7kbsz7g6ImGVl7gx6bl67NeL8Q&oe=62293959",
   },
   restoration: {
     title: "Restoration",
     description: "Restoration your vehicle with our quality paint.",
     image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxdMW4V_P5-cuVpDrJ_QiUvVr9FILL63IBKQ&usqp=CAU",
+      "https://scontent-dfw5-2.xx.fbcdn.net/v/t1.18169-9/13423886_1647995202106397_5767038844395986685_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=AArvP_ld9fQAX_fiI9Q&_nc_oc=AQlaK1ZYYxGyXfEORudIu9wmLykwdhIie-ZYDU3zTkOERUL3YpkDr3ApSwaLkyVezs8&_nc_ht=scontent-dfw5-2.xx&oh=00_AT8zJLlPZox-y1xFoKCyzI1dJQDcAEwZMRFvTy98bxYitw&oe=622881DE",
   },
 };
 
 const Section = () => {
   return (
-    <div className="section" id="Services">
-      <div className="section-inner">
-        <h1 className="section-header">Services</h1>
+    <div className={style.section} id="Services">
+      <div className={style.sectionInner}>
+        <h1 className={style.sectionHeader}>Services</h1>
       </div>
-      <div className="section-content">
-        <div className="section-cards">
-          {Object.keys(services).map((key) => (
-            <Card
-              key={key}
-              title={services[key].title}
-              description={services[key].description}
-              image={services[key].image}
-              className="section-card"
-            />
-          ))}
-        </div>
+
+      <div className={style.sectionCards}>
+        {Object.keys(services).map((key) => (
+          <Card
+            key={key}
+            title={services[key].title}
+            description={services[key].description}
+            image={services[key].image}
+          />
+        ))}
       </div>
     </div>
   );
--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -1,17 +1,37 @@
 import "./Card.css";
+import { useState } from "react";
+import { useSpring, a } from "@react-spring/web";
+import model from "./styles.module.css";
 
-const Card = ({ title, description, image }) => {
+const Card = (props) => {
+  const [flipped, set] = useState(false);
+  const { transform, opacity } = useSpring({
+    opacity: flipped ? 1 : 0,
+    transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
+    config: { mass: 10, tension: 400, friction: 60 },
+  });
+
   return (
-    <div className="card">
-      <div>
-        <img src={image} alt={title} className="card-image" />
-      </div>
-      <div className="card-text">
-        <h3>{title}</h3>
-        <div className="card-description">
-          <p className="text">{description}</p>
-        </div>
-      </div>
+    <div className={model.container} onClick={() => set((state) => !state)}>
+      <a.div
+        className={`${model.c} ${model.back}`}
+        style={{
+          backgroundImage: `url(${props.image})`,
+          opacity: opacity.to((o) => 1 - o),
+          transform,
+        }}
+        dangerouslySetInnerHTML={{ __html: props.title }}
+      />
+
+      <a.div
+        className={`${model.c} ${model.front}`}
+        style={{
+          opacity,
+          transform,
+          rotateX: "180deg",
+        }}
+        dangerouslySetInnerHTML={{ __html: props.description }}
+      />
     </div>
   );
 };
